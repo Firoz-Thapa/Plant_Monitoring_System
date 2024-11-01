b705018c109617304cdd5a4038d2094ad5883d74
Firoz Thapa <154414703+Firoz-Thapa@users.noreply.github.com>
2024-11-01 13:31:00 +0200

    Added python file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,24 +1,28 @@
+// src/firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";  // Import the Realtime Database
+// import { getAnalytics } from "firebase/analytics"; // Optional, only if you want analytics
 
-
-
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-  };
+  apiKey: "AIzaSyC5dpYtJ_zdQ7Ub8XrJ-S8ITGq5S0WaiFg",
+  authDomain: "plant-monitoring-system-6ef51.firebaseapp.com",
+  databaseURL: "https://plant-monitoring-system-6ef51-default-rtdb.europe-west1.firebasedatabase.app",  // Add this line for Realtime Database
+  projectId: "plant-monitoring-system-6ef51",
+  storageBucket: "plant-monitoring-system-6ef51.appspot.com",
+  messagingSenderId: "1073278823268",
+  appId: "1:1073278823268:web:3ce9f3d68c1b31a6c32871",
+  measurementId: "G-RWRCK18E82"
+};
 
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Initialize Realtime Database and export it
+const database = getDatabase(app);  // Initialize the Realtime Database
 
-const database = getDatabase(app); 
+// Optional: Initialize Analytics if you need it
+// const analytics = getAnalytics(app);
 
 export { database };
